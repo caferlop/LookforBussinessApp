0805aab83db0f0b3b84e7856a84aa4c0b61d7a35
Carlos Fernandez <carlos.fernandez@agilecontent.com>
2021-09-20 21:35:47 +0200

    Initial Commit

---
+++ LookForBussinessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LookForBussinessApp
+//
+//  Created by carlos fernandez on 20/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
